39743eaf3572d4bc99fd38427f8d1097d195d3b5
Lucas <lucasabreusilva.98@gmail.com>
2021-01-05 20:25:34 -0300

    Criado exercício do dia 2

---
+++ Desafio-100Dias-Exercicios/Dia_2.py
@@ -0,0 +1,24 @@
+"""
+                Calculadora de Troco
+
+1 - Crie um código que exiba uma mensagem de boas vindas ao usuário.
+2 - Pergunte ao usuário quanto deu a conta.
+3 - Pergunte ao usuário qual a % da gorjeta que ele quer pagar(crie algo que possa escolher entre 5, 10, 12 e 15.
+4 - Pergunte ao usuário quantas pessoas dividirão a gorjeta.
+5 - Exiba uma mensagem dizendo quanto cada pessoa deverá pagar.
+
+Exemplo:
+
+"Seja bem vindo(a) à Calculadora de Troco"
+
+"Qual o total da conta?"
+R$150,00
+
+"Quanto de gorjeta gostaria de deixar?"
+15
+
+"Quantas pessoas irão dividir a conta?"
+3
+
+"Cada pessoa deverá pagar uma quantia de: R$57,5"
+"""
