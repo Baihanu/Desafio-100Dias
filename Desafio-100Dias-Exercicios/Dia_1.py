cb62f784cf7a744461ccb40a9656efff5008a05d
Lucas <lucasabreusilva.98@gmail.com>
2021-01-05 19:48:03 -0300

    Criado exercício do dia 1

---
+++ Desafio-100Dias-Exercicios/Dia_1.py
@@ -0,0 +1,20 @@
+"""
+                Gerador Automático para nome de Banda.
+
+1 - Crie um código que exiba uma mensagem de boas vindas ao usuário
+2 - Pergunte ao usuário a cidade em que ele mora.
+3 - Pergunte ao usuário o nome de um animal de estimação.
+4 - Combine as duas perguntas em uma frase e exiba o nome da banda para o usuário.
+5 - Tenha certeza de que a entrada para a resposta apareça uma linha abaixo, conforme exemplo abaixo:
+
+Exemplo:
+
+Olá seja bem vindo ao Gerador Automático para nome de Banda.
+Pergunta = 'Por favor, informe o nome da sua Cidade:'
+Resposta = 'São Paulo"
+
+Pergunta = 'Por favor, informe o nome de um animal de estimação:'
+Resposta = 'Gato'
+
+Final = 'O Nome de sua banda é = "São Paulo Gato"'
+"""
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Desafio-100Dias
+# Desafio-100-Dias
+
+Este repositório busca trazer desafios para todos os níveis de desenvolvimento em Python.
+
+A ideia é realizar um desafio por dia, ao longo de 100 dias, desafiando a si mesmo com responsabilidade e autonomia.
+
+Cada projeto tem a própria descrição do que deve ser entregue.
+
+Eventualmente pretendo criar testes para testar automaticamente a solução desenvolvida.
+
+Sinta-se livre para indicar e compartilhar esse repositório com todo colega que estiver em busca de testar suas skills.
+
+Também são totalmente bem vindos, todo comentário e crítica sobre como e o que melhorar.
+
+Desde já, agradeço a atenção.
+
+Fiquem à vontade.
+
+Boa sorte!
+
+Vamos programar!!!
